e1066d7181be84bc884a064a9b8ff155a4d08c9e
Kostenkov-2021 <81031453+Kostenkov-2021@users.noreply.github.com>
2024-01-17 21:56:17 +0300

    Repository updates
    Code and documentation has been updated.

--- public/app.js
+++ public/app.js
@@ -1,146 +1,146 @@
 document.getElementById('loadButton').addEventListener('click', () => {
-    const folderPath = document.getElementById('folderPath').value;
-    // Отправляем запрос на сервер для получения данных
-    fetchFiles(folderPath);
+const folderPath = document.getElementById('folderPath').value;
+// Отправляем запрос на сервер для получения данных
+fetchFiles(folderPath);
 });
 
 // функция отправки запроса для получения данных
 function fetchFiles(folderPath) {
-    fetch('/read-html', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({ folderPath })
-    })
-    .then(response => response.json())
-    .then(data => displayHtmlFiles(data))
-    .catch(error => {
-        console.error('Error:', error);
-        showMessage('Ошибка при загрузке файлов', 'error');
-    });
+fetch('/read-html', {
+method: 'POST',
+headers: {'Content-Type': 'application/json'},
+body: JSON.stringify({ folderPath })
+})
+.then(response => response.json())
+.then(data => displayHtmlFiles(data))
+.catch(error => {
+console.error('Error:', error);
+showMessage('Ошибка при загрузке файлов', 'error');
+});
 }
 
 
 // Проверяем alt-текст на соответствие требованиям
 function validateAltText(altText, image) {
-    const warnings = [];
+const warnings = [];
 
-    // Проверка длины alt-текста
-    if (altText.length > 255) {
-        warnings.push(`Максимальная длина alt-текста составляет 255 символов, в вашем описании ${altText.length} символов.`);
-    } else if (altText.length > 125) {
-        warnings.push(`Рекомендуемая длина alt-текста составляет 125 символов, в вашем описании ${altText.length} символов.`);
-    }
+// Проверка длины alt-текста
+if (altText.length > 255) {
+warnings.push(`Максимальная длина alt-текста составляет 255 символов, в вашем описании ${altText.length} символов.`);
+} else if (altText.length > 125) {
+warnings.push(`Рекомендуемая длина alt-текста составляет 125 символов, в вашем описании ${altText.length} символов.`);
+}
 
-    // Проверка наличия точки в конце
-    if (!altText.endsWith('.')) {
-        warnings.push('В конце alt-текста рекомендуется ставить точку.');
-    }
+// Проверка наличия точки в конце
+if (!altText.endsWith('.')) {
+warnings.push('В конце alt-текста рекомендуется ставить точку.');
+}
 
-    // Проверка количества слов
-    const wordCount = altText.split(/\s+/).length;
-    if (wordCount < 3) {
-        warnings.push('Кажется, в вашем alt-тексте менее 3 слов. Рекомендуемая длина от 3 до 15 слов.');
-    } else if (wordCount > 15) {
-        warnings.push('Кажется, в вашем alt-тексте более 15 слов. Рекомендуемая длина от 3 до 15 слов.');
-    }
+// Проверка количества слов
+const wordCount = altText.split(/\s+/).length;
+if (wordCount < 3) {
+warnings.push('Кажется, в вашем alt-тексте менее 3 слов. Рекомендуемая длина от 3 до 15 слов.');
+} else if (wordCount > 15) {
+warnings.push('Кажется, в вашем alt-тексте более 15 слов. Рекомендуемая длина от 3 до 15 слов.');
+}
 
-    // Проверка начальных слов
-    if (/^(рисунок\|изображение\|картинка)\b/.test(altText.toLowerCase())) {
-        warnings.push('Alt-текст не нужно начинать словами "рисунок", "изображение" или "картинка", скринридер может делать это по умолчанию.');
-    }
+// Проверка начальных слов
+if (/^(рисунок\|изображение\|картинка)\b/.test(altText.toLowerCase())) {
+warnings.push('Alt-текст не нужно начинать словами "рисунок", "изображение" или "картинка", скринридер может делать это по умолчанию.');
+}
 
-    // Проверка на значок
+// Проверка на значок
 
-    // Убедимся, что размеры изображения загружены
-    if (image.complete && image.naturalWidth !== undefined) {
-        if (image.naturalWidth < 35 && image.naturalHeight < 35 && image.parentElement.className === "bodytext" && altText) {
-            warnings.push('Изображение похоже на значок. У значков не должно быть alt-текста, они всегда должны сопровождаться текстовым описанием.');
-        }
-    }
+// Убедимся, что размеры изображения загружены
+if (image.complete && image.naturalWidth !== undefined) {
+if (image.naturalWidth < 35 && image.naturalHeight < 35 && image.parentElement.className === "bodytext" && altText) {
+warnings.push('Изображение похоже на значок. У значков не должно быть alt-текста, они всегда должны сопровождаться текстовым описанием.');
+}
+}
 
     // Проверка на дублирование с подписью
-    const caption = image.parentElement.querySelector('.picturename');
-    if (caption && caption.textContent === altText) {
-        warnings.push('Alt-текст не должен дублировать подрисуночную подпись.');
-    }
+const caption = image.parentElement.querySelector('.picturename');
+if (caption && caption.textContent === altText) {
+warnings.push('Alt-текст не должен дублировать подрисуночную подпись.');
+}
 
-    return warnings;
+return warnings;
 }
 
 document.getElementById('filesContainer').addEventListener('input', (event) => {
-    if (event.target.tagName === 'INPUT' && event.target.type === 'text') {
-        const filePath = event.target.dataset.filepath;
-        const imgIndex = event.target.dataset.index;
-        const newAltText = event.target.value;
+if (event.target.tagName === 'INPUT' && event.target.type === 'text') {
+const filePath = event.target.dataset.filepath;
+const imgIndex = event.target.dataset.index;
+const newAltText = event.target.value;
 
-        // Валидация alt-текста и отображение предупреждений
-        const image = event.target.previousElementSibling; // Получаем изображение перед input
-        const warnings = validateAltText(newAltText, image);
-        displayWarnings(warnings, event.target); // Функция для отображения предупреждений
+// Валидация alt-текста и отображение предупреждений
+const image = event.target.previousElementSibling; // Получаем изображение перед input
+const warnings = validateAltText(newAltText, image);
+displayWarnings(warnings, event.target); // Функция для отображения предупреждений
 
-        updateAltText(filePath, imgIndex, newAltText);
-    }
+updateAltText(filePath, imgIndex, newAltText);
+}
 });
 
 
 
 function displayHtmlFiles(data) {
-    const filesContainer = document.getElementById('filesContainer');
-    filesContainer.innerHTML = '';
+const filesContainer = document.getElementById('filesContainer');
+filesContainer.innerHTML = '';
 
-    data.forEach(file => {
-        const fileName = file.filePath.split('/').pop().split('\\').pop();
-        const fileHeader = document.createElement('h2');
-        fileHeader.textContent = fileName;
-        filesContainer.appendChild(fileHeader);
+data.forEach(file => {
+const fileName = file.filePath.split('/').pop().split('\\').pop();
+const fileHeader = document.createElement('h2');
+fileHeader.textContent = fileName;
+filesContainer.appendChild(fileHeader);
 
-        if (file.imageData.length > 0) {
-            const table = createTable();
-            file.imageData.forEach((img, index) => {
-                const tr = createTableRow(img, file.filePath, index);
-                table.appendChild(tr);
-            });
-            filesContainer.appendChild(table);
-            const saveButton = createSaveButton(file.filePath, table);
-            filesContainer.appendChild(saveButton);
-        } else {
-            const noImagesMessage = document.createElement('p');
-            noImagesMessage.textContent = 'В этом файле нет изображений';
-            filesContainer.appendChild(noImagesMessage);
-        }
-    });
+if (file.imageData.length > 0) {
+const table = createTable();
+file.imageData.forEach((img, index) => {
+const tr = createTableRow(img, file.filePath, index);
+table.appendChild(tr);
+});
+filesContainer.appendChild(table);
+const saveButton = createSaveButton(file.filePath, table);
+filesContainer.appendChild(saveButton);
+} else {
+const noImagesMessage = document.createElement('p');
+noImagesMessage.textContent = 'В этом файле нет изображений';
+filesContainer.appendChild(noImagesMessage);
+}
+});
 }
 
 function createTable() {
-    const table = document.createElement('table');
-    const tableHeader = document.createElement('thead');
-    const headerRow = ['Название изображения', 'Текущий alt-текст', 'Рекомендации', 'Новый alt-текст']
-        .map(text => {
-            const th = document.createElement('th');
-            th.textContent = text;
-            return th;
-        });
+const table = document.createElement('table');
+const tableHeader = document.createElement('thead');
+const headerRow = ['Изображение', 'Текущий alt-текст', 'Рекомендации', 'Новый alt-текст']
+.map(text => {
+const th = document.createElement('th');
+th.textContent = text;
+return th;
+});
 
-    const tableHeaderRow = document.createElement('tr');
-    headerRow.forEach(th => tableHeaderRow.appendChild(th));
-    tableHeader.appendChild(tableHeaderRow);
-    table.appendChild(tableHeader);
+const tableHeaderRow = document.createElement('tr');
+headerRow.forEach(th => tableHeaderRow.appendChild(th));
+tableHeader.appendChild(tableHeaderRow);
+table.appendChild(tableHeader);
 
-    return table;
+return table;
 }
 
 function createTableRow(img, filePath, index) {
-    const tr = document.createElement('tr');
+const tr = document.createElement('tr');
 
-    const tdName = document.createElement('td');
-    const imgName = document.createElement('a');
-    imgName.href = img.src;
-    imgName.target = '_blank';
-    imgName.textContent = img.name;
-    tdName.appendChild(imgName);
+const tdName = document.createElement('td');
+const imgName = document.createElement('a');
+imgName.href = img.src;
+imgName.target = '_blank';
+imgName.textContent = img.name;
+tdName.appendChild(imgName);
 
-    const tdCurrentAlt = document.createElement('td');
-    tdCurrentAlt
+const tdCurrentAlt = document.createElement('td');
+tdCurrentAlt
 .textContent = img.alt;
 
 const tdRecommendation = document.createElement('td');
@@ -151,6 +151,8 @@ const newAltTextarea = document.createElement('textarea');
 newAltTextarea.dataset.filepath = filePath;
 newAltTextarea.dataset.index = index;
 newAltTextarea.value = img.alt;
+newAltTextarea.cols = 40;
+newAltTextarea.rows = 4;
 tdNewAlt.appendChild(newAltTextarea);
 
 tr.appendChild(tdName);
@@ -189,35 +191,36 @@ function gatherUpdatedDataFromTable(table, filePath) {
 return Array.from(table.querySelectorAll('tr')).slice(1).map(row => {
 const imgName = row.cells[0].textContent;
 const newAltText = row.cells[3].querySelector('textarea').value;
-return { filePath, imgName, newAltText };
+const imageIndex = row.cells[3].querySelector('textarea').dataset.index;
+return { filePath, imgName, newAltText, index: imageIndex };
 });
 }
 function getRecommendation(altText) {
-    const recommendations = [];
+const recommendations = [];
 
-    if (altText.length > 255) {
-        recommendations.push('Длина alt-текста превышает 255 символов. Рекомендуется сократить.');
-    } else if (altText.length > 125) {
-        recommendations.push('Длина alt-текста более 125 символов. Рекомендуется сделать его более лаконичным.');
-    }
+if (altText.length > 255) {
+recommendations.push('Длина alt-текста превышает 255 символов. Рекомендуется сократить');
+} else if (altText.length > 125) {
+recommendations.push('Длина alt-текста более 125 символов. Рекомендуется сократить');
+}
 
-    if (!altText.endsWith('.')) {
-        recommendations.push('В конце alt-текста рекомендуется ставить точку.');
-    }
+if (!altText.endsWith('.')) {
+recommendations.push('В конце alt-текста рекомендуется ставить точку');
+}
 
-    const wordCount = altText.split(/\s+/).length;
-    if (wordCount < 3) {
-        recommendations.push('Кажется, в alt-тексте менее 3 слов. Рекомендуется расширить описание.');
-    } else if (wordCount > 15) {
-        recommendations.push('Кажется, в alt-тексте более 15 слов. Рекомендуется сделать описание более кратким.');
-    }
+const wordCount = altText.split(/\s+/).length;
+if (wordCount < 3) {
+recommendations.push('Кажется, в alt-тексте менее 3 слов. Рекомендуется расширить описание');
+} else if (wordCount > 15) {
+recommendations.push('Кажется, в alt-тексте более 15 слов. Рекомендуется сократить');
+}
 
-    if (/^(рисунок\|изображение\|картинка)\b/.test(altText.toLowerCase())) {
-        recommendations.push('Не рекомендуется начинать alt-текст со слов "рисунок", "изображение" или "картинка".');
-    }
+if (/^(рисунок\|изображение\|картинка)\b/.test(altText.toLowerCase())) {
+recommendations.push('Не рекомендуется начинать alt-текст со слов "рисунок", "изображение" или "картинка"');
+}
 
 
-    return recommendations.length > 0 ? recommendations.join('. ') + '.' : 'Описание соответствует рекомендациям.';
+return recommendations.length > 0 ? recommendations.join('. ') + '.' : 'Описание соответствует рекомендациям.';
 }
 function showMessage(message, type) {
 const messageElement = document.createElement('div');
@@ -228,7 +231,7 @@ messageElement.textContent = message;
 document.body.appendChild(messageElement);
 
 setTimeout(() => {
-    document.body.removeChild(messageElement);
+document.body.removeChild(messageElement);
 }, 5000);
 }
 function displayWarnings(warnings, inputElement) {
--- server.js
+++ server.js
@@ -12,51 +12,51 @@ let htmlFilesFolderPath;
 // Статическое обслуживание файлов из папки 'public' и папки с HTML-файлами
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/files', (req, res, next) => {
-    if (htmlFilesFolderPath) {
-        express.static(htmlFilesFolderPath)(req, res, next);
-    } else {
-        next();
-    }
+if (htmlFilesFolderPath) {
+express.static(htmlFilesFolderPath)(req, res, next);
+} else {
+next();
+}
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
 // Маршрут для чтения HTML-файлов и извлечения данных изображений
 app.post('/read-html', async (req, res) => {
-    const folderPath = req.body.folderPath;
-    if (typeof folderPath !== 'string') {
-        return res.status(400).send('Invalid folder path');
-    }
+const folderPath = req.body.folderPath;
+if (typeof folderPath !== 'string') {
+return res.status(400).send('Invalid folder path');
+}
 
-    htmlFilesFolderPath = folderPath; // Обновляем текущий путь к папке
-    // Динамическое обслуживание статических файлов изображений
+htmlFilesFolderPath = folderPath; // Обновляем текущий путь к папке
+// Динамическое обслуживание статических файлов изображений
 
-    try {
-        const files = await fs.readdir(folderPath);
-        const htmlFiles = files.filter(file => file.endsWith('.html') || file.endsWith('.htm'));
-        const results = [];
+try {
+const files = await fs.readdir(folderPath);
+const htmlFiles = files.filter(file => file.endsWith('.html') || file.endsWith('.htm'));
+const results = [];
 
-        for (const file of htmlFiles) {
-            const filePath = path.join(folderPath, file);
-            const htmlContent = await fs.readFile(filePath, 'utf8');
-            const dom = new JSDOM(htmlContent);
-            const images = Array.from(dom.window.document.querySelectorAll('img'));
-            const imageData = images.map(img => ({
-                name: path.basename(img.src),
-                src: `/files/${path.basename(img.src)}`,
-                alt: img.getAttribute('alt') || ''
-            }));
+for (const file of htmlFiles) {
+const filePath = path.join(folderPath, file);
+const htmlContent = await fs.readFile(filePath, 'utf8');
+const dom = new JSDOM(htmlContent);
+const images = Array.from(dom.window.document.querySelectorAll('img'));
+const imageData = images.map(img => ({
+name: path.basename(img.src),
+src: `/files/${path.basename(img.src)}`,
+alt: img.getAttribute('alt') || ''
+}));
 
-            results.push({ filePath, imageData });
-        }
+results.push({ filePath, imageData });
+}
 
 
-    res.json(results);
+res.json(results);
 } catch (error) {
-    res.status(500).send('Error reading HTML files: ' + error.message);
+res.status(500).send('Error reading HTML files: ' + error.message);
 }
 });
 async function updateSingleAltText(filePath, index, newAltText) {
@@ -65,16 +65,16 @@ const htmlContent = await fs.readFile(filePath, 'utf8');
 const dom = new JSDOM(htmlContent);
 const images = dom.window.document.querySelectorAll('img');
 
-    if (index < 0 || index >= images.length) {
-        throw new Error('Invalid image index');
-    }
+if (index < 0 || index >= images.length) {
+throw new Error('Invalid image index');
+}
 
-    images[index].setAttribute('alt', newAltText);
+images[index].setAttribute('alt', newAltText);
 
-    const updatedHtml = dom.serialize();
-    await fs.writeFile(filePath, updatedHtml, 'utf8');
+const updatedHtml = dom.serialize();
+await fs.writeFile(filePath, updatedHtml, 'utf8');
 } catch (error) {
-    throw new Error(`Failed to update file ${filePath}: ${error.message}`);
+throw new Error(`Failed to update file ${filePath}: ${error.message}`);
 }
 }
 app.post('/save-updated-alt-texts', async (req, res) => {
